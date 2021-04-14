d3fb72688bf93344af69d4b786141c9ab4a21b4a
nidhisingh11 <nidhi.nitdgp@gmail.com>
2021-04-15 00:33:44 +0530

    initial commit

---
+++ src/main/java/com/example/dataflick/DataFlickApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataflick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFlickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataFlickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dataflick/DataFlickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataflick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFlickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
